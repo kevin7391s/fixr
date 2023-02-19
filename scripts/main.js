e7f3fbd3bfe6df5b9b7d10412f0058b90b6571ac
Kevin Smith <kevin341s@yahoo.com>
2023-02-19 16:33:27 -0500

    clean and comment js code
    cleaned and commented code

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,10 @@
+// store divs in variables
+const conversation = document.getElementById("conversation");
+const form = document.getElementById("chat-form");
+const input = document.getElementById("chat-input");
+
+// chat box, add text bubbles and text depending on chat sender
 function appendMessage(sender, message) {
-  const conversation = document.getElementById("conversation");
   const messageElem = document.createElement("div");
   messageElem.innerHTML = message;
 
@@ -8,21 +13,22 @@ function appendMessage(sender, message) {
   } else if (sender === "fixr") {
     messageElem.classList.add("fixr-response");
   }
-
   conversation.appendChild(messageElem);
 }
 
-const form = document.getElementById("chat-form");
-const input = document.getElementById("chat-input");
-
+// on button submit, take the users response and store in in variable to call function with
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  //user input message
   const message = input.value;
   input.value = "";
 
+  // call above function and display users message
   appendMessage("user", message);
 
+  // wait and send fixrs message
+  // second parameter is where real response needs to go
   setTimeout(() => {
     appendMessage("fixr", "I see.  Tell me more.");
   }, 1000);
