1bf67aec0859bd95421eaf16db61f9c51dee4a2f
Kevin Smith <kevin341s@yahoo.com>
2023-02-28 00:05:10 -0500

    update file
    //update changes
    //test fixed navbar mobile

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,5 @@
-// store divs in variables
-const conversation = document.getElementById("conversation");
-const form = document.getElementById("chat-form");
-const input = document.getElementById("chat-input");
-
-// chat box, add text bubbles and text depending on chat sender
 function appendMessage(sender, message) {
+  const conversation = document.getElementById("conversation");
   const messageElem = document.createElement("div");
   messageElem.innerHTML = message;
 
@@ -13,22 +8,21 @@ function appendMessage(sender, message) {
   } else if (sender === "fixr") {
     messageElem.classList.add("fixr-response");
   }
+
   conversation.appendChild(messageElem);
 }
 
-// on button submit, take the users response and store in in variable to call function with
+const form = document.getElementById("chat-form");
+const input = document.getElementById("chat-input");
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  //user input message
   const message = input.value;
   input.value = "";
 
-  // call above function and display users message
   appendMessage("user", message);
 
-  // wait and send fixrs message
-  // second parameter is where real response needs to go
   setTimeout(() => {
     appendMessage("fixr", "I see.  Tell me more.");
   }, 1000);
