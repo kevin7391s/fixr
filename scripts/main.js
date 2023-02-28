78145ef5b6899e4ec228c2322a803d20e0eff54c
Kevin Smith <kevin341s@yahoo.com>
2023-02-28 02:30:10 -0500

    more work on autoscroll
    added function in both function bodies

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,8 @@ const conversation = document.getElementById("conversation");
 const form = document.getElementById("chat-form");
 const input = document.getElementById("chat-input");
 
+let fixr_response = false;
+
 function appendMessage(sender, message) {
   const messageElem = document.createElement("div");
   messageElem.innerHTML = message;
@@ -11,12 +13,14 @@ function appendMessage(sender, message) {
   } else if (sender === "fixr") {
     messageElem.classList.add("fixr-response");
   }
+  conversation.scrollTop = conversation.scrollHeight;
 
   conversation.appendChild(messageElem);
 }
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  conversation.scrollTop = conversation.scrollHeight;
 
   const message = input.value;
   input.value = "";
@@ -25,11 +29,14 @@ form.addEventListener("submit", (event) => {
 
   setTimeout(() => {
     appendMessage("fixr", "I see.  Tell me more.");
+    conversation.scrollTop = conversation.scrollHeight;
   }, 1000);
 });
 
 const submitButton = document.getElementById("button-input");
 
-submitButton.addEventListener("click", function () {
+/*
+submitButton.addEventListener(fixr_response, function () {
   conversation.scrollTop = conversation.scrollHeight;
 });
+*/
